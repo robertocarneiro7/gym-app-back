440ff2beaf2a1a041c0cc75fe242b89aae64a9fc
rcfilho <roberto.silva@Consulting-for.edenred.com>
2021-08-25 11:21:03 -0300

    Starting api-exercise

---
+++ api-exercise/src/test/java/com/gym_app/apiexercise/ApiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym_app.apiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
